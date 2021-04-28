d2e4e2ad09bc773ebc79be86df0c8d9090ff151d
Kehomaxd <83215950+Kehomaxd@users.noreply.github.com>
2021-04-28 14:46:19 +0200

    Add files via upload

--- Mobile.ts
+++ Mobile.ts
@@ -4,14 +4,14 @@
 
 export class Mobile {
 
-    public name:string;
-    public model:string;
-    public trademark:string;
-    public sdSize:number;
-    public color:string;
-    public is5G:boolean;
-    public cameraNum: number;
-    public price:number;
+    private name:string;
+    private model:string;
+    private trademark:string;
+    private sdSize:number;
+    private color:string;
+    private is5G:boolean;
+    private cameraNum: number;
+    private price:number;
 
     constructor(namePrm:string, modelPrm:string, trademarkPrm:string, sdSizePrm:number, colorPrm:string, is5GPrm:boolean, cameraNumPrm:number, pricePrm:number){
 
@@ -26,19 +26,70 @@ export class Mobile {
 
     }
 
-    // printMobile():void{
+    public getName():string{
+        return this.name;
+    }
+    public getModel():string{
+        return this.model
+    }
+    public getTrademark():string{
+        return this.trademark;
+    }
+    public getSDsize():number{
+        return this.sdSize;    
+    }
+    public getColor():string{
+        return this.color;    
+    }
+    public get():boolean{
+        return this.is5G;
+    }
+    public getCameraNum():number{
+        return this.cameraNum;    
+    }
+    public getPrice():number{
+        return this.price;
+    }
 
-    //     console.log (
-    //         `Nombre: ${this.name}\n
-    //     Modelo: ${this.model}\n
-    //     Marca: ${this.trademark}\n
-    //     Memoria: ${this.sdSize}G\n
-    //     Color: ${this.color}\n
-    //     Es 5G: ${this.is5G}\n
-    //     Tiene: ${this.cameraNum} camaras
-    //     El precio es de: $${this.price}` )
 
-    // }
+    setName(change:string){
+        this.name = change;
+    }
+    setModel(change:string){
+        this.model = change;
+    }
+    setTrademark(change:string){
+        this.trademark = change;
+    }
+    setSDsize(change:number){
+        this.sdSize = change;
+    }
+    setColor(change:string){
+        this.color = change;
+    }
+    setIs5G(change:boolean){
+        this.is5G = change;
+    }
+    setCameraNum(change:number){
+        this.cameraNum = change;
+    }
+    setPrice(change:number){
+        this.price = change;
+    }
+
+    public printMobile(){
+
+        console.log (`The characteristics of the mobile are:\n
+        Nombre: ${this.name}\n
+        Modelo: ${this.model}\n
+        Marca: ${this.trademark}\n
+        Memoria: ${this.sdSize}G\n
+        Color: ${this.color}\n
+        Es 5G: ${this.is5G}\n
+        Tiene: ${this.cameraNum} camaras
+        El precio es de: $${this.price}` )
+
+    }
 
 
 
--- test2Mobile.ts
+++ test2Mobile.ts
@@ -1,18 +1,24 @@
-//@kehomaxd
-//Main
 
 import {Mobile} from "./Mobile";
 
 let cel1 = new Mobile ("Redmi Note 8T", "M1908C3XG", "Xiaomi", 8, "Blue", true, 5, 180);
 let cel2 = new Mobile ("Nokia 3210", "Unknow", "Nokia",  1, "Yellow", false, 0, 12);
 let cel3  = new Mobile ("Pixel 4a", "Unknow", "Google", 6, "Black", true, 2, 390); 
-let allCel = {cel1, cel2, cel3};
-console.log(cel1);
-console.log(cel2);
-console.log(cel3);
+let allCel = [cel1, cel2, cel3];
+// console.log(cel1);
+// console.log(cel2);
+// console.log(cel3);
 
-// cel2.is5G = true;
-// cel2.cameraNum = 5;
+cel1.printMobile();
+cel2.printMobile();
+cel3.printMobile();
 
-console.log(allCel);
-// cel1.printMobile();
+cel2.setIs5G(true);
+cel2.setCameraNum(4);
+
+
+for(let i=0; i<allCel.length; i++){
+
+    allCel[i].printMobile();
+}
+
